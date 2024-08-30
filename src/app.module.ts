3dfe654e128380dc6fa390039a95fd7cafe73df0
dgsong7077 <dgsong7077@gmail.com>
2024-08-30 17:09:41 +0900

    Mongoose 적용, User 모듈 생성, User 스키마 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest'),  // MongoDB 연결 설정
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
